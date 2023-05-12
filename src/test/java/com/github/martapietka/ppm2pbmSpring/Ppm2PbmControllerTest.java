5116def004e30fcf86feccc82053fcfc4d559b5b
MartaPietka <martazwolak9494@gmail.com>
2023-05-12 13:54:25 +0200

    Correct test

--- src/test/java/com/github/martapietka/ppm2pbmSpring/Ppm2PbmControllerTest.java
+++ src/test/java/com/github/martapietka/ppm2pbmSpring/Ppm2PbmControllerTest.java
@@ -2,26 +2,26 @@ package com.github.martapietka.ppm2pbmSpring;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+
+import org.springframework.http.MediaType;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
 class Ppm2PbmControllerTest {
 
-    @LocalServerPort
-    private int port;
-
     @Autowired
-    private TestRestTemplate restTemplate;
+    private MockMvc mockMvc;
 
     @Test
     public void shouldReturnCorrectImage() throws Exception {
@@ -29,14 +29,10 @@ class Ppm2PbmControllerTest {
         String imageName = "3x3.ppm";
         byte[] expectedBytes = Files.readAllBytes(Paths.get("src/test/resources/" + imageName));
         String expectedContentType = "image/x-portable-graymap";
-        String url = "http://localhost:" + port + "/image/" + imageName;
 
-        //WHEN
-        ResponseEntity response = restTemplate.getForEntity(url, byte[].class);
-
-        // THEN
-        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-        assertThat(response.getHeaders().getContentType().toString()).isEqualTo(expectedContentType);
-        assertThat(response.getBody()).isEqualTo(expectedBytes);
+        this.mockMvc.perform(get("/image/{imageName}", imageName))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.parseMediaType(expectedContentType)))
+                .andExpect(content().bytes(expectedBytes));
     }
 }
