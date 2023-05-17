93a2cc39211162818361d459032779db07150dcd
MartaPietka <martazwolak9494@gmail.com>
2023-05-17 17:30:14 +0200

    Correct Test

--- src/test/java/com/github/martapietka/ppm2pbmSpring/Ppm2PbmControllerTest.java
+++ src/test/java/com/github/martapietka/ppm2pbmSpring/Ppm2PbmControllerTest.java
@@ -5,12 +5,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.MediaType;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
 import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -23,12 +28,17 @@ class Ppm2PbmControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @MockBean
+    private ImageService imageService;
+
     @Test
     public void shouldReturnCorrectImage() throws Exception {
 
         String imageName = "3x3.ppm";
         byte[] expectedBytes = Files.readAllBytes(Paths.get("src/test/resources/" + imageName));
         String expectedContentType = "image/x-portable-graymap";
+
+        given(imageService.getImage(anyString())).willReturn(new ByteArrayResource(expectedBytes));
 
         this.mockMvc.perform(get("/image/{imageName}", imageName))
                 .andExpect(status().isOk())
